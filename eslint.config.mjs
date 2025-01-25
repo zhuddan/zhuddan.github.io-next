617658e1556b2ba92b9dde8dbc6ae94b2bdb680d
zhuddan <yobaci@163.com>
2025-01-25 11:03:35 +0800

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default antfu({
+  formatters: true,
+  react: true,
+  // 合并 Next.js 的配置
+  overrides: [
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ],
+})
