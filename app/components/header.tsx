fd8f58170402b4eff84c42c963d1e0eda6b4bd63
zhuddan <yobaci@163.com>
2025-02-03 04:40:44 +0800

    fix

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,24 @@
+'use client'
+import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function Header() {
+  const pathname = usePathname()
+  const menus = [
+    {
+      path: '/kb/about/a',
+      label: '知识库',
+      prefix: '/kb',
+    },
+    {
+      path: '/blog',
+      label: '博客',
+      prefix: '/blog',
+    },
+  ]
   return (
     <header
       className="h-[var(--header-height)] px-8 flex items-center border-b sticky top-0 bg-[var(--background)]"
@@ -18,8 +34,27 @@ export default function Header() {
         />
       </Link>
       <div className="flex-1"></div>
-      <Link href="/kb/about/a">知识库</Link>
-      <Link href="/blog" className="ml-1">博客</Link>
+      {
+        menus.map((item) => {
+          return (
+            <Link
+              key={item.path}
+              href={item.path}
+              className={
+                clsx(
+                  'ml-2 px-2 py-1 rounded flex items-center justify-center',
+                  pathname.startsWith(item.prefix)
+                    ? ' bg-primary-light bg-opacity-10 text-primary'
+                    : ' hover:bg-gray-50',
+                )
+              }
+            >
+              {item.label}
+            </Link>
+          )
+        })
+      }
+
     </header>
   )
 }
